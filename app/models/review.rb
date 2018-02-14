c8c1c6d9e16b95573bb1b005a326156e837542b4
Alex Deval <al3x.dvl@gmail.com>
2018-02-14 16:54:51 +0100

    reste un peu de css à faire

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
+  RATING = [0, 1, 2, 3, 4, 5]
+
   validates :content, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, inclusion: { in: RATING }
   validates :rating, numericality: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '397.693.1309',
+    category:     "chinese"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '(186)285-7925',
+    category:     "italian"
+  },
+  {
+    name:         'Imaginaire',
+    address:      '23 Rue Fautras, 29200 Brest',
+    phone_number: '02 98 43 30 13',
+    category:     "french"
+  },
+  {
+    name:         'Jimida',
+    address:      '5 Rue Louis Pasteur, 29200 Brest',
+    phone_number: '02 98 83 95 53',
+    category:     "japanese"
+  },
+  {
+    name:         'Le Lotus Bleu',
+    address:      '2 Rue Edouard Belin, 29200 Brest',
+    phone_number: '02 98 33 12 18',
+    category:     "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
