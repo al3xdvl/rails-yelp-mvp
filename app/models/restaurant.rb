8feabed4378f551f874b1fa04df9da9a17355945
Alex Deval <al3x.dvl@gmail.com>
2018-02-14 12:07:53 +0100

    modèles OK

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORY = [ "chinese", "italian", "french", "belgian" ]
-
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion { in: CATEGORY }
+  validates :category, inclusion: { in: [ "chinese", "italian", "japanese", "french", "belgian" ] }
 end
